7757fd59e37c872f1a990bacc41fd1e2c4836e6a
Matthew Cullen <m23.cullen@gmail.com>
2021-02-25 18:16:42 -0500

    add Model for Idol

---
+++ IdolCardAPI/Models/IdolCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdolCardAPI.Models
+{
+    public class IdolCard
+    {
+        public int IdolId { get; set; }
+        public string IdolName { get; set; }
+        public string IdolGroup { get; set; }
+        public string PhotoCardSet { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string PhotoFileName { get; set; }
+    }
+}
