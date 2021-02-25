a2a0424e8cf757d9d1010b173ad9cb597e5b3321
Matthew Cullen <m23.cullen@gmail.com>
2021-02-25 18:13:18 -0500

    add model for IdolGroup and property accesors

---
+++ IdolCardAPI/Models/IdolGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdolCardAPI.Models
+{
+    public class IdolGroup
+    {
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+    }
+}
