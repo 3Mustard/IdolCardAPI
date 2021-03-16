df300068b346ae99a521d466ea637067924571c6
Matthew Cullen <m23.cullen@gmail.com>
2021-03-16 18:58:26 -0400

    fix route attribute

--- IdolCardAPI/Controllers/IdolCardController.cs
+++ IdolCardAPI/Controllers/IdolCardController.cs
@@ -227,6 +227,7 @@ namespace IdolCardAPI.Controllers
         }
 
         [Route("GetAllGroupNames")]
+        [HttpGet]
         public JsonResult GetAllGroupNames()
         {
             string query = @"
